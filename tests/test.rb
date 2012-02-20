035aee63ac0a523ca67df1a295c0e8147ecb99f3
Jesper Thomschutz <jesper@jespersaur.com>
2012-02-20 18:11:52 +0000

    Seperating out functionality + start of a test

---
+++ tests/test.rb
@@ -0,0 +1,17 @@
+require './app'
+require 'test/unit'
+require 'rack/test'
+
+class JiraUserPainTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    JiraUserPain
+  end
+
+  def test_root
+    get '/'
+    assert_equal('Hello World', last_response.body)
+  end
+
+end
